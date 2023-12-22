63caea37a77bb4f40b2502a633da0056bc6e2b32
Sahil <sk2003311@gmail.com>
2023-12-22 17:13:51 +0530

    feature cards done

--- src/app/layout.js
+++ src/app/layout.js
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className='h-full bg-primary'>
       <body className={jost.className}>
-        <main className='relative flex flex-col min-h-screen'>
+        <main className='relative flex flex-col min-h-screen '>
           <div className='flex-grow flex-1'>{children}</div>
         </main>
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import Link from "next/link"
 export default function Home() {
   return (
     <>
-      <nav className="flex px-[1rem] py-[0.75rem] justify-between">
+      <nav className="w-full mx-auto flex px-[1rem] py-[0.75rem] justify-between">
 
           <Image
             src={logo}
@@ -23,25 +23,118 @@ export default function Home() {
       {/* Hero Section */}
       <div className="flex px-[1rem] py-[2rem] flex-col">
 
-        <div className="w-full my-[1rem]">
-            <h1 className="mx-auto px-[2rem] text-[2rem] font-extrabold text-text tracking-tight leading-8 text-center">Here Every <span className="text-accent">Bid</span> Tells a Story!</h1>
-            <h2 className="mx-auto mt-[1rem] px-[1rem] text-[1rem] text-text tracking-tight leading-5 text-center">Embark on an Adventure of Bidding, Bargains, and Unforgettable Finds!</h2>
-        </div>
+          <div className="w-full my-[1rem]">
+              <h1 className="mx-auto px-[2rem] text-[2rem] font-extrabold text-text tracking-tight leading-8 text-center">Here Every <span className="text-accent">Bid</span> Tells a Story!</h1>
+              <h2 className="mx-auto mt-[1rem] px-[1rem] text-[1rem] text-text tracking-tight leading-5 text-center">Embark on an Adventure of Bidding, Bargains, and Unforgettable Finds!</h2>
+          </div>
 
-        <div className="mt-[1rem] w-full rounded-xl shadow-[0px_0px_60px_8px_rgba(217,217,217,0.5)] bg-primary ">
-        <Image
-            src={hero}
-            width={1500}
-            
-            alt="logo"
-            />
-        </div>
+          <div className="mt-[1rem] w-full rounded-xl shadow-[0_8px_30px_rgb(0,0,0,0.05)] bg-primary ">
+          <Image
+              src={hero}
+              width={1500}
+
+              alt="logo"
+              className="rounded-xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] bg-primary"
+              />
+          </div>
+
       </div>
 
       {/* Feature Section */}
 
-      <div>
-        
+      <div className="flex px-[2rem] py-[2rem] flex-col items-center text-text">
+        <h1 className="text-[2rem] font-extrabold">How it <span className="text-accent">Works</span>!</h1>
+
+      {/* Feature Section Card Collection */}
+        <div className="flex flex-col items-stretch justify-center gap-[1.5rem] my-[1.5rem]">
+
+            {/* Feature Section Card 1 */}
+
+            <div className="flex flex-col items-center justify-center gap-[1rem] px-[1rem] py-[1.5rem] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"> 
+
+                <div className="bg-secondary w-[3.5rem] h-[3.5rem] rounded-full flex items-center justify-center">
+                  <div className="bg-accent w-[2.5rem] h-[2.5rem] rounded-full flex items-center justify-center">
+                    <h3 className="font-bold text-[1.25rem] text-primary">1</h3>
+                  </div>
+                </div>
+
+                <h1 className="font-bold text-[1.5rem] text-center">Sell with Ease</h1>
+
+                <p className="text-[0.875rem] text-center">List your products with detailed descriptions and set a starting bid.</p>
+
+            </div>
+
+            {/* Feature Section Card 2 */}
+
+            <div className="flex flex-col items-center justify-center gap-[1rem] px-[1rem] py-[1.5rem] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"> 
+
+                <div className="bg-secondary w-[3.5rem] h-[3.5rem] rounded-full flex items-center justify-center">
+                  <div className="bg-accent w-[2.5rem] h-[2.5rem] rounded-full flex items-center justify-center">
+                    <h3 className="font-bold text-[1.25rem] text-primary">2</h3>
+                  </div>
+                </div>
+
+                <h1 className="font-bold text-[1.5rem] text-center">Bid to Win</h1>
+
+                <p className="text-[0.875rem] text-center">Bidders engage in competitive offers to secure the best deals.</p>
+
+            </div>
+
+            {/* Feature Section Card 3 */}
+
+            <div className="flex flex-col items-center justify-center gap-[1rem] px-[1rem] py-[1.5rem] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"> 
+
+                <div className="bg-secondary w-[3.5rem] h-[3.5rem] rounded-full flex items-center justify-center">
+                  <div className="bg-accent w-[2.5rem] h-[2.5rem] rounded-full flex items-center justify-center">
+                    <h3 className="font-bold text-[1.25rem] text-primary">3</h3>
+                  </div>
+                </div>
+
+                <h1 className="font-bold text-[1.5rem] text-center">Time-limited Thrill</h1>
+
+                <p className="text-[0.875rem] text-center">Auctions are time-bound, ensuring quick and fair transactions.</p>
+
+            </div>
+
+            {/* Feature Section Card 4 */}
+
+            <div className="flex flex-col items-center justify-center gap-[1rem] px-[1rem] py-[1.5rem] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"> 
+
+                <div className="bg-secondary w-[3.5rem] h-[3.5rem] rounded-full flex items-center justify-center">
+                  <div className="bg-accent w-[2.5rem] h-[2.5rem] rounded-full flex items-center justify-center">
+                    <h3 className="font-bold text-[1.25rem] text-primary">4</h3>
+                  </div>
+                </div>
+
+                <h1 className="font-bold text-[1.5rem] text-center">Secure Transactions</h1>
+
+                <p className="text-[0.875rem] text-center">Highest bidder wins, and sellers receive payments hassle-free.</p>
+
+            </div>
+
+            {/* Feature Section Card 5 */}
+
+            <div className="flex flex-col grow items-center justify-center gap-[1rem] px-[1rem] py-[1.5rem] rounded-lg shadow-[0_3px_10px_rgb(0,0,0,0.2)]"> 
+
+                <div className="bg-secondary w-[3.5rem] h-[3.5rem] rounded-full flex items-center justify-center">
+                  <div className="bg-accent w-[2.5rem] h-[2.5rem] rounded-full flex items-center justify-center">
+                    <h3 className="font-bold text-[1.25rem] text-primary">5</h3>
+                  </div>
+                </div>
+
+                <h1 className="font-bold text-[1.5rem] text-center">That’s all, Now lets</h1>
+
+                <div className="flex gap-[0.5rem]">
+                    <Link className="rounded-md border-accent border-2 px-[0.5rem] py-[0.25rem] text-[14px] font-semibold text-accent hover:bg-accent hover:text-primary" href={'/'}>Start Selling</Link>
+                    <Link className="rounded-md border-accent border-2 px-[0.5rem] py-[0.25rem] text-[14px] font-semibold text-primary bg-accent hover:bg-primary hover:text-accent" href={'/'}>Start Bidding</Link>
+                </div>
+
+            </div>
+
+
+        </div>
+
+      
       </div>
     </>
   )
