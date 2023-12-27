f0f9863e744e45e33c93b3c63eb46b7d9e2a55bc
Sahil <sk2003311@gmail.com>
2023-12-27 16:36:18 +0530

    setup done for login & signup

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,10 +1,12 @@
-import { connect } from "@/dbConfig/dbConfig"
+
 
 export default function LoginPage() {
 
-    connect();
 
     return(
-        <h1>Login</h1>
+        <>
+        
+        </>
+        
     )
 }
---
+++ src/app/signup/page.jsx
@@ -0,0 +1,8 @@
+
+export function SignUpPage(){
+    return(
+        <>
+            
+        </>
+    )
+}
